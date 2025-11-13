7bf53029844237a37a0f44f10bc51a3ede0b6580
Asanbek09 <samudinua@gmail.com>
2025-11-13 16:10:19 +0600

    Добавлен /cmd/api/error.go

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,13 +7,14 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
 		"status": "available",
-		"environment": app.config.env,
-		"version": version,
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
