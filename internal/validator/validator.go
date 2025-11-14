02a7c20c58433d8f1ed65164e4066f059aaa4890
Asanbek09 <samudinua@gmail.com>
2025-11-14 13:00:35 +0600

    Использование валидации в cmd/api/movies.go

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	EmailRX = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]{1,256}@[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}(?:\.[a-zA-Z0-9][a-zA-Z0-9\-]{0,64})+$`)
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9]{0,61}[a-zA-Z0-9])?)*$")
 )
 
 type Validator struct {
