d9f5d4b150fbfc267d506373f8cf117b981c39fa
Asanbek09 <samudinua@gmail.com>
2025-11-17 16:57:35 +0600

    Запись кино в базу данных

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"movielight/internal/validator"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -38,7 +40,9 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `insert into movies(title, year, runtime, genres) values ($1, $2, $3, $4) returning id, created_at, version`
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)} 
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*MovieModel, error) {
