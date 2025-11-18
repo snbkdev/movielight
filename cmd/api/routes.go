e92a5f2fbd8aedac0e0a4677fbb4e68717b485a4
Asanbek09 <samudinua@gmail.com>
2025-11-18 11:28:18 +0600

    Обновление фильма

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,5 +16,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMoviehandler)
+
 	return app.recoverPanic(router)
 }
