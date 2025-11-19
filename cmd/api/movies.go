739da4de34738f545381263af9ba83f7b1794073
Asanbek09 <samudinua@gmail.com>
2025-11-19 17:28:37 +0600

    Создан /internal/data/filters.go

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -176,9 +176,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	var input struct {
 		Title string
 		Genres []string
-		Page int
-		PageSize int
-		Sort string
+		data.Filters
 	}
 
 	v := validator.New()
@@ -188,10 +186,10 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Title = app.readString(qs, "title", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
 
-	input.Page = app.readInt(qs, "page", 1, v)
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
-	input.Sort = app.readString(qs, "sort", "id")
+	input.Filters.Sort = app.readString(qs, "sort", "id")
 
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
