0901af398db83f03d54ebca6666dae4e0433518e
Asanbek09 <samudinua@gmail.com>
2025-11-25 15:25:21 +0600

    Добавлены некоторые функции для токен

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,9 +1,12 @@
 package data
 
 import (
+	"context"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
 	"math/rand"
+	"movielight/internal/validator"
 	"time"
 )
 
@@ -39,4 +42,45 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 	token.Hash = hash[:]
 	
 	return token, nil
+}
+
+func ValidateTokenPlainText(v *validator.Validator, tokenPlainText string) {
+	v.Check(tokenPlainText != "", "token", "must be provided")
+	v.Check(len(tokenPlainText) == 26, "token", "must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `insert into tokens(hash, user_id, expiry, scope) values($1, $2, $3, $4)`
+
+	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	query := `delete from tokens where scope = $1 and user_id = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+	return err
 }
