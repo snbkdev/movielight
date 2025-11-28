6c8b17863e1a5860ffdd49648a1e58926748facb
Asanbek09 <samudinua@gmail.com>
2025-11-28 15:50:36 +0600

    Завершение проекта

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -8,6 +8,7 @@ import (
 	"movielight/internal/validator"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -208,11 +209,48 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	})
 }
 
+type metricsResponseWriter struct {
+	wrapped http.ResponseWriter
+	statusCode int
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped: w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+	return mw.wrapped
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	var (
 		totalRequestsReceived = expvar.NewInt("total_requests_received")
 		totalResponsesSent = expvar.NewInt("total_responses_sent")
 		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_us")
+
+		totalResponsesSentByStatus = expvar.NewMap("total_responses_sent_by_status")
 	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -220,9 +258,13 @@ func (app *application) metrics(next http.Handler) http.Handler {
 
 		totalRequestsReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		mw := newMetricsResponseWriter(w)
+
+		next.ServeHTTP(mw, r)
 
 		totalResponsesSent.Add(1)
+
+		totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
 		duration := time.Since(start).Microseconds()
 		totalProcessingTimeMicroseconds.Add(duration)
